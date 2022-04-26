d0b45f176d7899af043aa90068e80724f45fdafc
Tomáš Reinert <reinerttomas@gmail.com>
2022-04-26 21:45:55 +0200

    debug & asset

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,18 +13,7 @@ class HomeController extends AbstractController
     #[Route('/')]
     public function homepage(): Response
     {
-        $tracks = [
-            ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
-            ['song' => 'Waterfalls', 'artist' => 'TLC'],
-            ['song' => 'Creep', 'artist' => 'Radiohead'],
-            ['song' => 'Kiss from a Rose', 'artist' => 'Seal'],
-            ['song' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
-            ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
-        ];
-        return $this->render('home/homepage.html.twig', [
-            'title' => 'PB & Jams',
-            'tracks' => $tracks,
-        ]);
+        return $this->render('home/homepage.html.twig');
     }
 
     #[Route('/browse/{slug}')]
