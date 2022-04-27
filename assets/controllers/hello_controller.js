11a6ab05ea0e111584551f21d7e03a35a41c273b
Tomáš Reinert <reinerttomas@gmail.com>
2022-04-27 19:12:11 +0200

    stimulus

--- assets/controllers/hello_controller.js
+++ assets/controllers/hello_controller.js
@@ -1,16 +1,12 @@
 import { Controller } from '@hotwired/stimulus';
 
-/*
- * This is an example Stimulus controller!
- *
- * Any element with a data-controller="hello" attribute will cause
- * this controller to be executed. The name "hello" comes from the filename:
- * hello_controller.js -> "hello"
- *
- * Delete this file or adapt it for your use!
+/**
+ * @property {HTMLElement} titleTarget
  */
 export default class extends Controller {
-    connect() {
-        this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+    static targets = ['title'];
+
+    greet() {
+        this.titleTarget.textContent = 'Hello Stimulus!';
     }
 }
